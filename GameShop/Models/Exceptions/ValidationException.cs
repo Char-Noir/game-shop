0b85922a3aec44b503f92bc6cca30959369f0ae5
CharNoir <dburyak2002@gmail.com>
2022-06-11 10:26:08 +0300

    GMBX-10 GMBX-11 GMBX-10 GMBX-13 GMBX-14 add custom exceptions

---
+++ GameShop/Models/Exceptions/ValidationException.cs
@@ -0,0 +1,20 @@
+﻿namespace GameShop.Models.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public string Type { get; } = string.Empty;
+        public ValidationException()
+        {
+        }
+
+        public ValidationException(string message, string type) : base(message)
+        {
+            Type = type;
+        }
+
+        public ValidationException(string message, string type, Exception innerException) : base(message, innerException)
+        {
+            Type = type;
+        }
+    }
+}
