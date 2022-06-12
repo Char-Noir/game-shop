b5340db00cda8bb29806904fa3121a02d805936e
CharNoir <dburyak2002@gmail.com>
2022-06-12 18:00:48 +0300

    GMBX-10 GMBX-11 GMBX-12 GMBX-13 GMBX-14 add basic files

---
+++ GameShop/wwwroot/js/pagination.js
@@ -0,0 +1,9 @@
+﻿function sendNewPage(number) {
+    let form = $('#pagination_form');
+    const input = document.createElement("input");
+    input.type = "number";
+    input.name = "currentPage";
+    input.value = number;
+    form.append(input);
+    form.submit();
+}
