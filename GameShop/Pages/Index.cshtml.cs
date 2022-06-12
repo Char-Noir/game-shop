e5646f80f75b11ab8c77bfcb42d43f9721df7a41
CharNoir <dburyak2002@gmail.com>
2022-06-12 17:54:44 +0300

    GMBX-10 GMBX-11 GMBX-12 GMBX-13 GMBX-14 add index page

---
+++ GameShop/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using GameShop.Models.Entity;
+using GameShop.Models.Entity.RequestEntity;
+using GameShop.Models.Service.Interface;
+using GameShop.Models.Utils.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameShop.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IProductService _productService;
+
+        public IndexModel(ILogger<IndexModel> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+        
+        public IList<Product> Product { get; set; } 
+        
+        public async Task<IActionResult> OnGet()
+        {
+            var all = await _productService.GetAll();
+            Product =  all.Take(4).ToList();
+            
+            return Page();
+        }
+    }
+}
