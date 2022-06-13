8aa3dd5f7e54abeb67b3a764859d76531d305d96
CharNoir <dburyak2002@gmail.com>
2022-06-13 12:41:21 +0300

    GMBX-13 add details page

---
+++ GameShop/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿#nullable disable
+using GameShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GameShop.Models.Entity;
+using GameShop.Models.Utils;
+
+namespace GameShop.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly GameShopContext _context;
+
+        public DetailsModel(GameShopContext context)
+        {
+            _context = context;
+        }
+
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product = await _context.Product.Include(m=>m.ProductTypes).ThenInclude(x=>x.Product_Type).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            Product.InitProductWithCategories();
+            Product.InitPackageList();
+            return Page();
+        }
+    }
+}
