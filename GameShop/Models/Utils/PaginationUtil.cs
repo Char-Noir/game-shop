9b4dfe6c4bb831b6421320f4d11a47144e0437d2
CharNoir <dburyak2002@gmail.com>
2022-06-13 16:17:16 +0300

    GMBX-14 GMBX-11 add utils for pagination

---
+++ GameShop/Models/Utils/PaginationUtil.cs
@@ -0,0 +1,41 @@
+﻿namespace GameShop.Models.Utils;
+
+public static class PaginationUtil
+{
+    public static int[] GetAvaliablePages(int totalPages, int currentPage)
+    {
+        int[] result;
+        if (totalPages < 5)
+        {
+            result=new int[totalPages];
+            for (var i = 0; i < totalPages; i++)
+            {
+                result[i] = i + 1;
+            }
+            return result;
+        }
+        result = new int[5];
+        if (currentPage < 3)
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                result[i] = i + 1;
+            }    
+        }
+        else if (currentPage > totalPages - 2)
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                result[5-1-i] = totalPages - i;
+            }  
+        }
+        else
+        {
+            for (var i = -2; i < 3; i++)
+            {
+                result[i + 2] = currentPage + i;
+            }
+        }
+        return result;
+    }
+}
